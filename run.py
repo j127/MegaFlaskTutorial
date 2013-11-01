8802be932c377bc6a73a195e79ddf90ae422b64b
J. Cohen <hostelmarketing@gmail.com>
2013-10-31 19:04:45 -0700

    Create base.html template

--- run.py
+++ run.py
@@ -1,8 +1,4 @@
 #!flask/bin/python
-import os
 from app import app
 app.run(debug=True)
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
